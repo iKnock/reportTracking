8f4c073800d79f11e0cfe2d653f43fd335435add
iKnock <ergana.henok@gmail.com>
2020-08-27 21:28:21 +0200

    exception handling

--- models/woreda.js
+++ models/woreda.js
@@ -1,11 +1,14 @@
+"use strict";
+
 const dbConnection = require('../utils/connection');
+const AppExceptions = require('../utils/appError');
 
 class Woreda {
 	constructor(objectId, regionName, zoneName, woredaNo, woredaName, globalId) {
 		this.objectId = objectId;
 		this.regionName = regionName;
-        this.zoneName = zoneName;
-        this.woredaNo = woredaNo;
+		this.zoneName = zoneName;
+		this.woredaNo = woredaNo;
 		this.woredaName = woredaName;
 		this.globalId = globalId;
 	}
@@ -14,9 +17,9 @@ class Woreda {
 
 	getObjectId() {
 		return this.objectId;
-    }
-    
-    getRegionName() {
+	}
+
+	getRegionName() {
 		return this.regionName;
 	}
 
@@ -30,44 +33,55 @@ class Woreda {
 
 	getWoredaName() {
 		return this.woredaName;
-    }        
+	}
 
-    getGlobalId(){
-        return this.globalId;
-    }
-	
+	getGlobalId() {
+		return this.globalId;
+	}
+
 	getWoreda() {
 		return {
 			objectId: this.objectId,
 			regionName: this.regionName,
 			zoneName: this.zoneName,
-            woredaNo: this.woredaNo,
-            woredaName: this.woredaName,			
+			woredaNo: this.woredaNo,
+			woredaName: this.woredaName,
 			globalId: this.globalId
-		};		
-	}
-	
+		};
+	}	
+
 	listAllWoreda = function (callback) {
-		dbConnection.getConnection(function (connection) {
-			connection.query('SELECT * FROM tbl_woreda', function (error, results, fields) {
-				var woredas = [];
-				for (var i in results) {
-					if (results.hasOwnProperty(i)) {
-						const woreda = new Woreda(
-							results[i].OBJECT_ID,
-							results[i].REGION_NAME,
-							results[i].ZONE_NAME,
-							results[i].WOREDA_NO_,
-							results[i].WOREDA_NAME,
-							results[i].Global_ID
-						);
-						woredas.push(woreda)
+		dbConnection.getConnection(function (connection, conError) {
+			if (conError != null) {
+				const conErr = new AppExceptions(conError.code, conError.message);
+				callback(null, conErr);
+			} else {
+				connection.query('SELECT * FROM tbl_woreda', function (error, results, fields) {
+					try {
+						var woredas = [];
+						for (var i in results) {
+							if (results.hasOwnProperty(i)) {
+								const woreda = new Woreda(
+									results[i].OBJECT_ID,
+									results[i].REGION_NAME,
+									results[i].ZONE_NAME,
+									results[i].WOREDA_NO_,
+									results[i].WOREDA_NAME,
+									results[i].Global_ID
+								);
+								woredas.push(woreda)
+							}
+						}
+						callback(woredas, null);
+						connection.release();// When done with the connection, release it.                    
+						if (error) throw error;// Handle error after the release.  
+					} catch (error) {
+						//write in the log the original exception and return readable format to the caller
+						const err = new AppExceptions(error.code, error.message);
+						callback(null, err);
 					}
-				}
-				callback(woredas);
-				connection.release();// When done with the connection, release it.                    
-				if (error) throw error;// Handle error after the release.        				
-			});
+				});
+			}
 		})
 	};
 }
