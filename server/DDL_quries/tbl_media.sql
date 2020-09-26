afe007cc4d3eca6d0c86ad640086607599855f00
iKnock <ergana.henok@gmail.com>
2020-09-26 18:21:32 +0200

    add model definition

--- server/DDL_quries/tbl_media.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `db_reporting_app`.`tbl_media`;
-CREATE TABLE IF NOT EXISTS `db_reporting_app`.`tbl_media` (
-  `media_id` int(11) NOT NULL AUTO_INCREMENT,
-  `evidence_id` int(11) NOT NULL,
-  `media_url` varchar(50) NOT NULL,
-  `description` varchar(200) NOT NULL,
-  PRIMARY KEY (`media_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
--- server/DDL_quries/tbl_user_report.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS `db_reporting_app`.`tbl_user_report`;
-CREATE TABLE IF NOT EXISTS `db_reporting_app`.`tbl_user_report` (
-  `user_report_id` int(11) NOT NULL AUTO_INCREMENT,
-  `report_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `description` varchar(200) NOT NULL,
-  PRIMARY KEY (`user_report_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
